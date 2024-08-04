3c1d22c184c44e326a3f65c554706a807b0295f9
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2024-08-04 14:33:50 +0530

    force dynamic api route

--- app/api/fetchtransactions/route.ts
+++ app/api/fetchtransactions/route.ts
@@ -1,19 +1,36 @@
+// import prisma from "@/lib/prisma";
+// import { NextRequest, NextResponse } from "next/server";
+
+
+// async function fetchTransactions(request: NextRequest) {
+//   try {
+//     const transactions = await prisma.transaction.findMany({
+//         orderBy: {
+//             createdAt: 'desc'
+//         }
+//     })
+//     return NextResponse.json({ data: transactions }, { status: 200 });
+//   } catch (err: any) {
+//     console.log(err);
+//     return NextResponse.json({ error: err.message }, { status: 500 });
+//   }
+// }
+
+// export { fetchTransactions as GET };
+
 import prisma from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
-
-
-async function fetchTransactions(request: NextRequest) {
-  try {
-    const transactions = await prisma.transaction.findMany({
-        orderBy: {
-            createdAt: 'desc'
-        }
-    })
-    return NextResponse.json({ data: transactions }, { status: 200 });
-  } catch (err: any) {
-    console.log(err);
-    return NextResponse.json({ error: err.message }, { status: 500 });
-  }
+import { NextResponse } from "next/server";
+export const dynamic = 'force-dynamic'
+export async function GET() {
+    try {
+        const transactions = await prisma.transaction.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+        return NextResponse.json({ data: transactions }, { status: 200 });
+    } catch (err: any) {
+        console.log(err);
+        return NextResponse.json({ error: err.message }, { status: 500 });
+    }
 }
-
-export { fetchTransactions as GET };
